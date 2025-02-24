406eab9ffb30eca9845048b66a0664109db32857
clara-ab <clara.ab.adds@gmail.com>
2025-02-24 19:50:19 +0000

    24-02-2025 -- 2

--- streamlit_main.py
+++ streamlit_main.py
@@ -5,9 +5,9 @@ import streamlit as st
 def main ():
 
     # Título:
-    #st.markdown("<h1 style = 'text-align: center'; color: #dc793a; font-family: \'Droid Sans Mono\', monospace;'> 🚗 Compra y Vende tu Coche - Coches de Segunda Mano 🚗 </h1>", unsafe_allow_html=True);
+    st.markdown("<h1 style = 'text-align: center'; color: #dc793a; font-family: \'Droid Sans Mono\', monospace;'> 🚗 Compra y Vende tu Coche - Coches de Segunda Mano 🚗 </h1>", unsafe_allow_html=True);
     #st.title("🚗 Compra y Vende tu Coche - Coches de Segunda Mano 🚗")
-    st.write ("Hola")
+    #st.write ("Hola")
 
     # Subtítulo: 
     #st.markdown("h3 style = 'text-align: center'; color: black; font-family: \'Droid Sans Mono\', monospace;'> \n\n¿Quieres comprar o vender?\n\n </h3>");
