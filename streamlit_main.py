3b93ad5e2d42ee0e9155bb79ae55b2288a2961b7
clara-ab <clara.ab.adds@gmail.com>
2025-02-24 19:39:06 +0000

    24-02-2025 -- 1

---
+++ streamlit_main.py
@@ -0,0 +1,14 @@
+# Librería para pdoer utilizar Streamlit
+import streamlit as st
+
+# # # MAIN FUNCTION # # #
+def main ():
+
+    # Título:
+    st.markdown("<h1 style = 'text-align: center'; color: #dc793a; font-family: \'Droid Sans Mono\', monospace;'> 🚗 Compra y Vende tu Coche - Coches de Segunda Mano 🚗 </h1>", unsafe_allow_html=True);
+    #st.title("🚗 Compra y Vende tu Coche - Coches de Segunda Mano 🚗")
+
+    # Subtítulo: 
+    #st.markdown("h3 style = 'text-align: center'; color: black; font-family: \'Droid Sans Mono\', monospace;'> \n\n¿Quieres comprar o vender?\n\n </h3>");
+
+  
