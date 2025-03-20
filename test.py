9c4921fdfbe77acb0d12eb170f735a2e19455976
clara-ab <clara.ab.adds@gmail.com>
2025-03-20 18:45:58 +0000

    testing 20-03-2025

---
+++ test.py
@@ -0,0 +1,10 @@
+# # # # # INICIO LIBRERÍAS # # # # #
+
+# Librería para pdoer utilizar Streamlit
+import streamlit as st
+
+# Librería para poder cambiar de páginas de visualización:
+from streamlit_extras.switch_page_button import switch_page
+
+# # # # #  FIN LIBRERÍAS # # # # #
+
